cafa50986d313fcf67f37cec97c87510f1cf429e
Alexandre Kajihara <alexandre.ykz@gmail.com>
2017-06-20 09:34:07 -0300

    Predescessor ok

--- libraries/todosospares.c
+++ libraries/todosospares.c
@@ -19,16 +19,25 @@ void floyd(){
 						2, INT_MAX, -5, 0, INT_MAX,
 						INT_MAX, INT_MAX, INT_MAX, 6, 0};
 
+	long int matrizZero[5][5], matrizZeroR[5][5];
 
-
+	for(i = 0; i < 5; i++){
+		for(j = 0; j < 5; j++){
+			if(i == j || matriz[i][j] == INT_MAX){
+				matrizZero[i][j] = -1;
+			} else if(i != j && matriz[i][j] < INT_MAX){
+				matrizZero[i][j] = i;
+			}
+		}
+	}
+/*
 	for(l = 0; l < 5; l++){
 		for(m = 0; m < 5; m++){
-			printf("%ld ", matriz[l][m]);
+			printf("%ld ", matrizZero[l][m]);
 		}
 		printf("\n");
 	}
-
-	printf("\n");
+*/
 	long int matrizR[5][5];
 
 	for(k = 0; k < 5; k++){
@@ -37,17 +46,24 @@ void floyd(){
 				if(i == j){
 					matrizR[i][j] = 0;
 				} else {
-					printf("i: %d\n", i);
-					printf("j: %d\n", j);
-					printf("mat: %d\n", matriz[i][j]);
-					printf("mat: %d/%d\n", matriz[i][k], matriz[k][j]);					
 					matrizR[i][j] = minimo(matriz[i][j], matriz[i][k] + matriz[k][j]);	
+				}
+				if(matriz[i][j] > matriz[i][k] + matriz[k][j]){
+					matrizZeroR[i][j] = matrizZero[k][j];
+				} else if(matriz[i][j] <= matriz[i][k] + matriz[k][j]){
+					matrizZeroR[i][j] = matrizZero[i][j];
 				}
 			}
 		}
 		for(l = 0; l < 5; l++){
 			for(m = 0; m < 5; m++){
 				matriz[l][m] = matrizR[l][m];
+			}
+			//printf("\n");
+		}
+		for(l = 0; l < 5; l++){
+			for(m = 0; m < 5; m++){
+				matrizZero[l][m] = matrizZeroR[l][m];
 			}
 			//printf("\n");
 		}
@@ -60,5 +76,13 @@ void floyd(){
 		}
 		printf("\n");
 	}
+	printf("\n");
+	for(l = 0; l < 5; l++){
+		for(m = 0; m < 5; m++){
+			printf("%ld ", matrizZero[l][m]);
+		}
+		printf("\n");
+	}
+//	predescessores(matriz);
 
 }
